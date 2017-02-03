f62fcc2498f94e0a9310d9187698c4d554b812b7
isa <isaliangyiyun@hotmail.com>
2017-02-02 20:20:52 -0500

    fix header causing hadoop not found file

--- post_answer_length_mapper.py
+++ post_answer_length_mapper.py
@@ -1,4 +1,4 @@
-#!/usu/bin/python
+#!/usr/bin/python
 
 import sys
 import csv
--- post_answer_length_reducer.py
+++ post_answer_length_reducer.py
@@ -1,27 +1,29 @@
-#!/usu/bin/python
+#!/usr/bin/python
 
 import sys
 
-old_post = None
+old_post_id = None
+old_post_len = None
 count_len = 0.0
 count = 0
 
 for line in sys.stdin:
     data = line.strip().split()
 
-    if len(data) != 2:
+    if len(data) != 3:
         continue
 
-    this_post, this_ans = data
+    this_post_id, this_post_len, this_ans_len = data
 
-    if old_post and old_post != this_post:
-        print "{0}\t{1}".format(this_post, count_len/count)
+    if old_post_id and old_post_id != this_post_id:
+        print "{0}\t{1}\t{2}".format(this_post_id, this_post_len, count_len/float(count))
         count_len = 0
         count = 0
 
-    old_post = this_post
-    count_len += len(this_ans)
+    old_post_id = this_post_id
+    old_post_len = this_post_len
+    count_len += int(this_ans_len)
     count += 1
 
-if old_post != None:
-    print "{0}\t{1}".format(this_post, count_len/count)
+if old_post_id != None:
+    print "{0}\t{1}\t{2}".format(this_post_id, this_post_len, count_len/float(count))
